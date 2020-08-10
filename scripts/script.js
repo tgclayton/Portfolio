ae73f7da9976e2a677ac7d83880c5305712744ce
Tom Clayton <tgclayton1@gmail.com>
2020-08-10 12:00:40 +1200

    add larger project window to appear project page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
 const messageWindow = document.getElementById('message-window')
+const bigProject = document.getElementById('big-project-box')
+
+// for (const i in projectBoxes) {
+//   const project = projectBoxes[i]
+//   console.log(project)
+//   project.addEventListener('click', toggleBigProject())
+// }
+
+document.querySelectorAll('.project-box').forEach(item => {
+  item.addEventListener('click', () => toggleBigProject())
+})
 
 function toggleSendMessage () {
   messageWindow.classList.toggle('hidden')
+}
+
+function toggleBigProject () {
+  bigProject.classList.toggle('hidden')
 }
